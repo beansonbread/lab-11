50fe226ec44610cde3123168fe1a7efb0fd6a70a
atento <96808973+beansonbread@users.noreply.github.com>
2025-04-21 23:39:55 -0400

    create subclass PerishableProductProperties

--- script.js
+++ script.js
@@ -13,4 +13,16 @@ class ProductProperties {
     }
 }
 
+class PerishableProductProperties extends ProductProperties {
+    constructor(name, price, quantity, expirationDate) {
+        super(name, price, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    toString() {
+        return console.log(`${super.toString()}, Expiration Date: ${this.expirationDate}`);
+    }
+}
+
+
 
