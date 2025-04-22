b666d48d3cd056316da4b5180fd246a9790b1837
atento <96808973+beansonbread@users.noreply.github.com>
2025-04-21 23:39:16 -0400

    create class ProductProperties

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity; 
+    }
+    getTotalValue() {
+        return this.price * this.quantity;
+    }
+
+    toString() {
+        return console.log(`Product Name: ${this.name}, Price: ${this.price}, Quantity: ${this.quantity}`);
+    }
+}
+
 
